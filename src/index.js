5349f40308cac7c533b4d6b947691f3ca46a07cf
jihye <kwisdom95@naver.com>
2024-02-13 10:40:49 +0900

    ss

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import express from "express"
 const app=express();
 
+
 const PORT=3000
 
 // app.get("/",(req,res)=>{
 //     res.send("root direstory")
 // })
 app.get("/",(req,res)=>{
-    res.send("Hello World!");
+    res.send("Hello World! github push");
 });
 
 app.listen(PORT,()=> console.log(`Sever on: http://localhost:${PORT}`));
